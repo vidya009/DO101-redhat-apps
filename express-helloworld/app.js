94326d839d3a8633b4ebd021ca686050aa25c22f
Vidya Bhojane <vidya.bhojane@ibm.com>
2024-02-26 16:39:59 +0530

    Add /mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+// new feature: to display Hello Mars! when a user accesses the /mars application endpoint.
+app.get('/mars', function(req, res) {
+  res.send('Hello Mars!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
